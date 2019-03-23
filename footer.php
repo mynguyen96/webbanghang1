cfec41110dd7a3074d8d5a91eb7eaefed76606d2
mynguyen96@gmail.com <nguyenthihoaimyit@gmail.com>
2019-03-23 17:30:29 +0700

    test

---
+++ footer.php
@@ -0,0 +1,7 @@
+	</div>
+	<footer>
+		&#169; 2019 - Mai - <a href="https://donghoxiteen.com" target="_blank">donghoxiteen.com</a>
+	</footer>
+	<script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
